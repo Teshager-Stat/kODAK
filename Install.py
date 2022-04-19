f96e86755b23cbf0423c65e3d0c1042598130b6c
Teshestat <teshagerzm@gmail.com>
2022-04-19 16:06:17 +0300

    first line plot

---
+++ Install.py
@@ -0,0 +1,6 @@
+from tokenize import PlainToken
+import matplotlib.pyplot as plt
+x=[21,23,45,45]
+y=[21,54,32,45]
+plt.plot(x,y)
+plt.show()
--- May.py
+++ May.py
@@ -1,1 +1,13 @@
-print("Hello world")
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import io
+import imageio
+from IPython.display import Image, display
+from ipywidgets import widgets, Layout, HBox
